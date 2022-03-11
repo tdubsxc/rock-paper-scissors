4722e5e8e60e281cbeb9efb1c86c6f5dacf7e76a
tdubsxc <tdubsxc@gmail.com>
2022-03-10 21:38:31 -0500

    Setup the overlay and rules modal.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,27 @@
 // * Variables
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const rulesBtn = document.querySelector('#rules');
+const closeModalBtn = document.querySelector('#close-modal');
+
 // * Methods
-// * Events
-window.addEventListener('DOMContentLoaded', () => {
+function pageLoad(_e) {
   document.body.classList.remove('preload');
-});
+  document
+    .querySelector('meta[name=viewport]')
+    .setAttribute('content', this.viewport.content + ', height=' + window.innerHeight);
+}
+
+function showModal(e) {
+  overlay.classList.add('visible');
+}
+
+function closeModal(e) {
+  overlay.classList.remove('visible');
+}
+
+// * Events
+window.addEventListener('DOMContentLoaded', pageLoad);
+window.addEventListener('keydown', (e) => overlay.classList.contains('visible') && e.key === 'Escape' && closeModal());
+rulesBtn.addEventListener('click', showModal);
+closeModalBtn.addEventListener('click', closeModal);
