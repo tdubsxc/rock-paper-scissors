b125e8bba0f2101a69de4d20fb59587e276a995e
tdubsxc <tdubsxc@gmail.com>
2022-03-14 18:15:13 -0400

    Added js to compare user pick with ai pick and clean up.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,71 @@
 // * Variables
-const overlay = document.querySelector('.overlay');
-const modal = document.querySelector('.modal');
+const viewport = document.querySelector('meta[name=viewport]');
+const overlayEl = document.querySelector('.overlay');
 const rulesBtn = document.querySelector('#rules');
 const closeModalBtn = document.querySelector('#close-modal');
+const paperBtn = document.querySelector('#paper');
+const gameEl = document.querySelector('#game');
+const resultsEl = document.querySelector('#results');
+const pickBtns = document.querySelectorAll('#game button');
+const resultsOutcomeEls = document.querySelectorAll('#results div');
+
+const picks = [
+  {
+    name: 'paper',
+    beats: 'rock',
+  },
+  {
+    name: 'scissors',
+    beats: 'paper',
+  },
+  {
+    name: 'rock',
+    beats: 'scissors',
+  },
+];
 
 // * Methods
-function pageLoad(_e) {
+function pageLoad() {
   document.body.classList.remove('preload');
-  document
-    .querySelector('meta[name=viewport]')
-    .setAttribute('content', this.viewport.content + ', height=' + window.innerHeight);
+  viewport.setAttribute('content', viewport.content + ', height=' + window.innerHeight);
 }
 
-function showModal(e) {
-  overlay.classList.add('visible');
+function showModal() {
+  overlayEl.classList.add('visible');
 }
 
-function closeModal(e) {
-  overlay.classList.remove('visible');
+function closeModal() {
+  overlayEl.classList.remove('visible');
+}
+
+function handleUserPick() {
+  const pickName = this.dataset.pick;
+  const userPick = picks.find((pick) => pick.name === pickName);
+  compare(userPick);
+}
+
+function handleAIPick() {
+  return picks[Math.trunc(Math.random() * picks.length)];
+}
+
+function compare(userPick) {
+  const aiPick = handleAIPick();
+  displayResultsUI([userPick, aiPick]);
+}
+
+function displayResultsUI(results) {
+  console.log(results);
 }
 
 // * Events
-window.addEventListener('DOMContentLoaded', pageLoad);
-window.addEventListener('keydown', (e) => overlay.classList.contains('visible') && e.key === 'Escape' && closeModal());
+window.addEventListener('load', pageLoad);
+window.addEventListener('keydown', (e) => {
+  if (overlayEl.classList.contains('visible') && e.key === 'Escape') {
+    closeModal();
+  }
+});
 rulesBtn.addEventListener('click', showModal);
 closeModalBtn.addEventListener('click', closeModal);
+pickBtns.forEach((pickBtn) => {
+  pickBtn.addEventListener('click', handleUserPick);
+});
