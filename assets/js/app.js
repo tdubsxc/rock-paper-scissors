0c2d050a3bbe9c18c30142e048858d8a2af8dbb5
tdubsxc <tdubsxc@gmail.com>
2022-03-18 15:42:12 -0400

    Added functionality to determine a winner.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,16 @@ const viewport = document.querySelector('meta[name=viewport]');
 const overlayEl = document.querySelector('.overlay');
 const rulesBtn = document.querySelector('#rules');
 const closeModalBtn = document.querySelector('#close-modal');
-const paperBtn = document.querySelector('#paper');
-const gameEl = document.querySelector('#game');
-const resultsEl = document.querySelector('#results');
-const pickBtns = document.querySelectorAll('#game button');
-const resultsOutcomeEls = document.querySelectorAll('#results div');
+const gameEl = document.querySelector('.game');
+const pickBtns = document.querySelectorAll('.game button');
+const resultsEl = document.querySelector('.results');
+const resultsEls = document.querySelectorAll('.results__result');
+const resultsMsgEl = document.querySelector('#results__msg');
+const resultsStatusEl = document.querySelector('.results__status');
+const playAgainBtn = document.querySelector('#play-again');
+const scoreEl = document.querySelector('#score');
+
+let score = 0;
 
 const picks = [
   {
@@ -31,11 +36,11 @@ function pageLoad() {
 }
 
 function showModal() {
-  overlayEl.classList.add('visible');
+  overlayEl.classList.add('overlay--visible');
 }
 
 function closeModal() {
-  overlayEl.classList.remove('visible');
+  overlayEl.classList.remove('overlay--visible');
 }
 
 function handleUserPick() {
@@ -50,22 +55,64 @@ function handleAIPick() {
 
 function compare(userPick) {
   const aiPick = handleAIPick();
-  displayResultsUI([userPick, aiPick]);
+  showResultsUI([userPick, aiPick]);
+  showWinnerUI([userPick, aiPick]);
 }
 
-function displayResultsUI(results) {
-  console.log(results);
+function showResultsUI(results) {
+  resultsEls.forEach((resultEl, i) => {
+    setTimeout(() => {
+      resultEl.innerHTML = `
+        <div class="hand hand--bigger ${results[i].name}">
+          <div class="hand-inner">
+            <img src="/assets/images/icon-${results[i].name}.svg" alt="${results[i].name}" />
+          </div>
+        </div>
+    `;
+    }, i * 500);
+  });
+
+  gameEl.classList.toggle('game--hidden');
+  resultsEl.classList.toggle('results--hidden');
+}
+
+function showWinnerUI(results) {
+  setTimeout(() => {
+    const userWins = determineWinner(results);
+    const aiWins = determineWinner(results.reverse());
+
+    if (userWins) {
+      resultsMsgEl.innerHTML = 'YOU WIN';
+      resultsEls[0].classList.add('results__result--winner');
+      // updateScore(1);
+    } else if (aiWins) {
+      resultsMsgEl.innerHTML = 'YOU LOSE';
+      resultsEls[1].classList.add('results__result--winner');
+      // updateScore(-1);
+    } else {
+      resultsMsgEl.innerHTML = 'DRAW';
+    }
+
+    resultsEl.classList.toggle('results--final');
+    resultsStatusEl.classList.toggle('results__status--hidden');
+  }, 500);
+}
+
+function determineWinner(results) {
+  return results[0].beats === results[1].name;
 }
 
 // * Events
 window.addEventListener('load', pageLoad);
 window.addEventListener('keydown', (e) => {
-  if (overlayEl.classList.contains('visible') && e.key === 'Escape') {
-    closeModal();
-  }
+  overlayEl.classList.contains('overlay--visible') && e.key === 'Escape' && closeModal();
 });
-rulesBtn.addEventListener('click', showModal);
+overlayEl.addEventListener('click', (e) => {
+  e.target.classList.contains('overlay--visible') && closeModal();
+});
 closeModalBtn.addEventListener('click', closeModal);
+rulesBtn.addEventListener('click', showModal);
 pickBtns.forEach((pickBtn) => {
   pickBtn.addEventListener('click', handleUserPick);
 });
+playAgainBtn.addEventListener('click', playAgain);
